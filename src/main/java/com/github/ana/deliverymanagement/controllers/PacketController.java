19c35ad7f9f72d36525827f0bd6e7fa733de2758
salimataana <ana.salimata.sanou@gmail.com>
2024-10-19 14:03:02 +0200

    Update : implements the packet creation and redirect to list of packet.

--- src/main/java/com/github/ana/deliverymanagement/controllers/PacketController.java
+++ src/main/java/com/github/ana/deliverymanagement/controllers/PacketController.java
@@ -4,11 +4,14 @@ package com.github.ana.deliverymanagement.controllers;
 import com.github.ana.deliverymanagement.models.Packet;
 import com.github.ana.deliverymanagement.repository.PacketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -30,18 +33,20 @@ public class PacketController {
     }
     @RequestMapping(value="/packet/create",method= RequestMethod.GET)
     public String create(Model model){
-        model.addAttribute("packet", new Packet()); // A
+        //model.addAttribute("packet", new Packet()); // A
         return "createPacket";
     }
 
     @RequestMapping(value="/packet",method= RequestMethod.POST)
-    public String store (@RequestParam("name") String name, @RequestParam("address_packet") String address_packet,  @RequestParam("date_depart") Date date_depart){
+    public ModelAndView store (@RequestParam("name") String name, @RequestParam("address_packet") String address_packet,
+                               @RequestParam("date_depart") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date_depart){
+        System.out.println("je suis dans store");
         Packet packet = new Packet();
         packet.setName(name);
         packet.setAddress_packet(address_packet);
         packet.setDate_depart(date_depart);
         packetRepository.save(packet);
-        return "redirect:/packet";
+        return new ModelAndView("redirect:/packet");
     }
     @RequestMapping(value="/packet/{id}",method= RequestMethod.GET)
     public String show(Integer id){
